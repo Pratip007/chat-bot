5fa0778ba118854c4a15f27ea4d3d9aa7f5601a3
Pratip Maity <pratipmaity500@gmail.com>
2025-05-23 10:40:44 +0530

    proxy add

--- admin/src/environments/environment.ts
+++ admin/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  API_URL: 'https://api.urbanwealthcapitals.com/api',
-  SOCKET_URL: 'https://api.urbanwealthcapitals.com/'
+  API_URL: '/api',
+  SOCKET_URL: '/'
 };
